e7e5cf4bdd6f047aab5900658964ddd3b4771f61
Ravi Kiran HR <ravi.kiran@philips.com>
2022-02-15 11:06:21 +0530

    FeedItem Model is created

---
+++ EssentialFeeds/EssentialFeeds/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeeds
+//
+//  Created by Ravi Kiran HR on 15/02/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    var id: UUID
+    var description: String?
+    var location: String?
+    var imageURL: URL
+}
