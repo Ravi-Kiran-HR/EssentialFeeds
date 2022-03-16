a4fcda90157197ceb7abcc93cdc619833e14dd1e
Ravi Kiran HR <Ravi Kiran>
2022-03-16 17:15:27 +0530

    Added End to end Integration tests for remote feed loader

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -8,9 +8,18 @@
 import Foundation
 
 public struct FeedItem: Equatable {
+    
+    public init(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+    
     var id: UUID
     var description: String?
     var location: String?
     var imageURL: URL
+    
 }
 
